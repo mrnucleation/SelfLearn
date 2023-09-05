97199182ae48326a94f99100360d8aacbd37b37b
Troy Loeffler <troydloeffler@gmail.com>
2023-09-05 14:55:01 -0700

    Wrote the trial portion of the script.

--- MCTS_Trial.py
+++ MCTS_Trial.py
@@ -2,18 +2,38 @@ from time import time
 from MCTSOpt.MCTree import Tree
 from MCTSOpt.ParameterOpt.UniformSearch import UniformSearch
 import numpy as np
+from math import fabs
+# =============================================================================
+class FunctionWrapper:
+    def __init__(self, function) -> None:
+        self.function = function
+        self.bestscore = None
+        self.evalcount = 0
+    # --------------------------------------------------------
+    def __call__(self, parameters):
+        self.evalcount += 1
+        score = self.function(parameters)
+        if self.bestscore is None:
+            self.bestscore = score
+        else:
+            self.bestscore = min(self.bestscore, score)
+        return score
+    # --------------------------------------------------------
+    
 
 #===============================================================================
 def runtrial(testfunction, selectionrule):
 
+    wrappedfunction = FunctionWrapper(testfunction)
     depthscale = [1.0, 0.7, 0.5, 0.3, 0.2, 0.1]
     depthlimit = len(depthscale)+5
     ubounds = testfunction.ubounds
     lbounds = testfunction.lbounds
+    f_target = testfunction.f_best
     startset = np.random.uniform(lbounds, ubounds, size=ubounds.shape[0])
 
     indata = UniformSearch(startset, lbounds, ubounds, depthscale=depthscale)
-    indata.setevaluator(testfunction)
+    indata.setevaluator(wrappedfunction)
 
 
     #---Tree Main Run loop---
@@ -30,7 +50,12 @@ def runtrial(testfunction, selectionrule):
         tree.playexpand(nExpansions=1, depthlimit=depthlimit)
         tree.autoscaleconstant(scaleboost=2.0)
         tree.simulate(nSimulations=1)
-        curmin = tree.getbestscore()
+        curmin = wrappedfunction.bestscore
+        if fabs(curmin - f_target) < 1e-4:
+            break
         curtime = time()
         print("Search Duration: %s, Best Score:%s"%(curtime-starttime, curmin))
+    bestscore = wrappedfunction.bestscore   
+    evalcount = wrappedfunction.evalcount 
+    return bestscore, evalcount
 #===============================================================================
